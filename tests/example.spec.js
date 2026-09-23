65e0aadec7ebeec6629585051eaca0ca2c08d4b7
Ganesh-186 <ganeshravichandran06@gmail.com>
2026-09-23 16:58:30 +0530

    Adding all files

---
+++ tests/example.spec.js
@@ -0,0 +1,8 @@
+import {test} from '@playwright/test';
+import { LoginPage} from '../pages/Login';
+import user from '../data/user-data.json'
+test('SauceDemo',async({page})=>{
+  const Login = new LoginPage(page);
+  await Login.login(user.user1.name,user.password);
+  await page.waitForTimeout(4000);
+})
