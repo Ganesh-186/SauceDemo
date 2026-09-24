f651e103cd2e6e4c98ea6545fcb448fa967791a2
Ganesh-186 <ganeshravichandran06@gmail.com>
2026-09-24 13:54:34 +0530

    User4 changed

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,6 +3,6 @@ import { LoginPage} from '../pages/Login';
 import user from '../data/user-data.json'
 test('SauceDemo',async({page})=>{
   const Login = new LoginPage(page);
-  await Login.login(user.user3.name,user.password);
+  await Login.login(user.user4.name,user.password);
   await page.waitForTimeout(4000);
 })
