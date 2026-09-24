394fc46498fe747e2bb929b42830cc6bdd786c7b
Ganesh-186 <ganeshravichandran06@gmail.com>
2026-09-24 14:47:56 +0530

    Train first commit

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,8 +1,16 @@
 import {test} from '@playwright/test';
 import { LoginPage} from '../pages/Login';
 import user from '../data/user-data.json'
+
 test('SauceDemo',async({page})=>{
   const Login = new LoginPage(page);
   await Login.login(user.user6.name,user.password);
+  await page.waitForTimeout(4000);
+})
+
+//added by Basker -- Train module
+test('Train Scenario',async({page})=>{
+  const Login = new LoginPage(page);
+  await Login.login(user.user5.name,user.password);
   await page.waitForTimeout(4000);
 })
